0c8bf1db1d33233c6ed981df06a99ac3ee031729
thaynar96 <thaynarbo@gmail.com>
2021-10-07 23:20:49 -0300

    shared component - Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as Style from './styles';
+const Header = () => {
+	return (
+		<Style.Container>
+			<img src='' alt='' />
+			<Link></Link>
+			<Link></Link>
+		</Style.Container>
+	);
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { theme } from './../../styles/themes';
+
+export const Container = styled.div`
+	background: ${theme.colors.black30};
+	height: 40px;
+`;
