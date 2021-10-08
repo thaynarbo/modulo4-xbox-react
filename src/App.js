b96010a8ef468f9a0292825965b9f4778b5ddb83
thaynar96 <thaynarbo@gmail.com>
2021-10-07 23:22:52 -0300

    Routing and global styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import UserPage from './pages/UserPage';
 import Details from './pages/Details';
 import GlobalStyle from './styles/reset';
+import CreateAccount from './pages/CreateAccount';
 function App() {
 	return (
 		<>
 			<GlobalStyle />
 			<Switch>
+				<Route path='/signup' component={CreateAccount}></Route>
 				<Route path='/' exact={true} component={UserPage} />
 				<Route path='/details/:id' component={Details} />
 			</Switch>
