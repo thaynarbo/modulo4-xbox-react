35a2dcbf58b354484916017a7993f5c6fb01951d
thaynar96 <thaynarbo@gmail.com>
2021-10-07 23:20:30 -0300

    input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as Style from './style';
+const Input = ({ text, icon, ...props }) => {
+	return (
+		<Style.Container>
+			<Style.ContainerInput
+				{...props}
+				icon={icon}
+				placeholder={text}
+			></Style.ContainerInput>
+			{icon && icon}
+		</Style.Container>
+	);
+};
+
+export default Input;
---
+++ src/components/Input/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { theme } from './../../styles/themes';
+
+export const Container = styled.div`
+	position: relative;
+
+	svg {
+		position: absolute;
+		top: 50%;
+		left: 0;
+		transform: translate(0, -50%);
+		margin-left: 0.5rem;
+		font-size: 1rem;
+	}
+`;
+
+export const ContainerInput = styled.input`
+	height: 20px;
+	padding: 1.4rem;
+	border-radius: 0.7rem;
+	border: none;
+	width: 100%;
+	font-size: ${theme.fontSizes.p1};
+	padding-left: ${({ icon }) => icon && theme.spacing.space5};
+
+    :
+`;
