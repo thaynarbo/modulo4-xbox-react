10c3d6373882a789b4da83855a9abffc71aae11f
thaynar96 <thaynarbo@gmail.com>
2021-10-07 23:20:17 -0300

    page to sign in

---
+++ src/pages/CreateAccount/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Style from './styles';
+import Form from './../../components/Form';
+const CreateAccount = () => {
+	return (
+		<Style.Container>
+			<Form />
+		</Style.Container>
+	);
+};
+
+export default CreateAccount;
---
+++ src/pages/CreateAccount/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { theme } from './../../styles/themes';
+
+export const Container = styled.div`
+	height: 100vh;
+	width: 100vw;
+	display: grid;
+	place-items: center;
+	padding: 1rem;
+`;
