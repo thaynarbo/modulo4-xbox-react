ab591c9b0849a537b7019fa667c10806ab8f1a10
thaynar96 <thaynarbo@gmail.com>
2021-10-20 13:49:07 -0300

    chore(cleanup): remove andd move components

--- src/components/Header/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { theme } from './../../styles/themes';
-
-export const Container = styled.div`
-	background: ${theme.colors.black30};
-	height: 40px;
-`;
--- src/pages/UserPage/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import GameList from '../../components/GameList';
-const UserPage = () => {
-	return <GameList />;
-};
-
-export default UserPage;
