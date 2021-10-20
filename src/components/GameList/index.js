b10797b1df35444d7985a9cca669a2c9e12b5c4a
thaynar96 <thaynarbo@gmail.com>
2021-10-20 15:26:24 -0300

    feat(game): GameList component

--- src/components/GameList/index.js
+++ src/components/GameList/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import * as Style from './styles';
-import Card from '../Card';
+import React, { useState, useEffect } from "react";
+import * as Style from "./styles";
+import Card from "../Card";
+import { Api } from "./../../api/api";
 const GameList = () => {
-	return (
-		<Style.Container>
-			<Card />
-			<Card />
-		</Style.Container>
-	);
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    const loadGameList = async () => {
+      const response = await Api.buildApiGetRequest(Api.readAllGames(), true);
+      const result = await response.json();
+      if (result.length <= 0) {
+        setGames("");
+      } else {
+        setGames(result);
+      }
+    };
+    loadGameList();
+  }, []);
+  if (!games) {
+    return <h1 style={{ color: "#fff" }}>Não há jogos para mostrar</h1>;
+  }
+  return (
+    <Style.Container>
+      {games.map((game) => {
+        return <Card game={game} key={game.id} />;
+      })}
+    </Style.Container>
+  );
 };
 
 export default GameList;
