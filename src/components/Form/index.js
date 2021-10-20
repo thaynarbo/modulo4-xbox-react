7d0fe033bcf524d42b7b81eb23faab051fffa4e4
thaynar96 <thaynarbo@gmail.com>
2021-10-20 15:13:36 -0300

    feat(login): login form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import * as Style from './styles';
-import Input from './../Input';
-import { AiOutlineUser } from 'react-icons/ai';
-import { RiLock2Fill } from 'react-icons/ri';
-import Button from '../Button';
+import React from "react";
+import * as Style from "./styles";
+import Input from "../Input";
+import { AiOutlineUser } from "react-icons/ai";
+import { RiLock2Fill } from "react-icons/ri";
+import Button from "../Button";
+import { Api } from "../../api/api";
 
-const Form = () => {
-	return (
-		<Style.Container>
-			<div className='input-container'>
-				<Input text='Usuário' icon={<AiOutlineUser />} />
-				<Input text='Senha' icon={<RiLock2Fill />} />
-			</div>
-			<Button text='Entrar' green={true} />
-		</Style.Container>
-	);
+const Form = (props) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const payload = {
+      email,
+      password,
+    };
+
+    const response = await Api.buildApiPostRequest(Api.loginUrl(), payload);
+    const body = await response.json();
+    if (response.status === 200) {
+      const accessToken = body.accessToken;
+      localStorage.setItem("JWT", accessToken);
+      props.history.push("/home");
+    }
+  };
+  return (
+    <Style.Container onSubmit={handleSubmit}>
+      <div className="input-container">
+        <Input name="email" id="email" text="Email" icon={<AiOutlineUser />} />
+        <Input
+          name="password"
+          type="password"
+          id="password"
+          text="Senha"
+          icon={<RiLock2Fill />}
+        />
+      </div>
+      <Button text="Entrar" green={true} />
+    </Style.Container>
+  );
 };
 
 export default Form;
